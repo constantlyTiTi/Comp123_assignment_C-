ef3c2ed4ec544bd3ef923695c4a1a1a1aa7b0515
Ting Li <tingli@Ting-MacBook-Pro.local>
2019-02-10 01:18:28 -0500

    moved to folder

---
+++ Assignmet_Class/MenuItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assignment01
+{
+    public class MenuItem
+    {
+        //private string name;
+        private string description;
+        private decimal baseCost;
+        private string name;
+
+        public string Description { get { return description; } 
+                                    set { description = value; } }
+        public decimal BaseCost { get { return baseCost; } }
+        public string Name { get { return name; }}
+
+        public MenuItem(string name,decimal cost)
+        {
+            this.name = name;
+            baseCost = cost;
+        }
+        public void GetInfor()
+        {
+            Console.Write("||{0,8}", name);
+            Console.Write("{0,13:C2}", baseCost);
+            Console.Write("{0,20} ||", description);
+            Console.WriteLine();
+        }
+    }
+}
