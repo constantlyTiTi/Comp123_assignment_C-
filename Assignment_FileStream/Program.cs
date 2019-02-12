0e95aab197b49bc32946207c2ef0b7c8cf5b311b
Ting Li <tingli@Ting-MacBook-Pro.local>
2019-02-11 23:08:40 -0500

    explain the difference between OpenOrCreate and Create command

--- Assignment_FileStream/Program.cs
+++ Assignment_FileStream/Program.cs
@@ -28,7 +28,8 @@ namespace Assignment_FileStream
             {
                 Console.WriteLine("File [{0}] exists", fileName);
                 courseFile = new FileStream
-                    (fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                   // (fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite); --will open file and edit on the file, use could set from which line to be updated
+                   (fileName, FileMode.Create, FileAccess.ReadWrite); //will truncate the intend file
             }
             else
             {
