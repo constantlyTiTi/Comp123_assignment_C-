a8787222bb67f9695d9534c97605651f3644b774
Ting Li <tingli@Ting-MacBook-Pro.local>
2019-02-17 12:39:46 -0500

    add another method to do withdraw

--- Assignment_03_BankAccount/Assignment_03_BankAccount/CheckingAccount.cs
+++ Assignment_03_BankAccount/Assignment_03_BankAccount/CheckingAccount.cs
@@ -9,21 +9,32 @@ namespace Assignment_03_BankAccount
             string checkAccountNo = base.GetAccountNumber() + "-" + "10";
             this.SetAccountNumber(checkAccountNo);
         }
+        #region FirstWay to do Widthdraw
+        //public override Boolean Withdraw(double amount)
+        //{
+        //    Boolean withdrawComplete;
+
+        //    if (GetBalance() - amount - fee >= 0)
+        //    {
+        //        withdrawComplete = true;
+        //        SetBalance(GetBalance() - amount - fee);
+        //    }
+        //    else
+        //    {
+        //        withdrawComplete = false;
+        //    }
+
+        //    return withdrawComplete;
+        //}
+        #endregion
+        #region secondWay to do Widthdraw
         public override Boolean Withdraw(double amount)
         {
-            Boolean withdrawComplete;
-
-            if (GetBalance() - amount -fee>= 0)
-            {
-                withdrawComplete = true;
-                SetBalance(GetBalance() - amount - fee);
-            }
-            else
-            {
-                withdrawComplete = false;
-            }
-
-            return withdrawComplete;
+            double restBalance;
+            restBalance = this.GetBalance() - amount - fee;
+            SetBalance(restBalance);
+            return base.Withdraw(restBalance);
         }
+        #endregion
     }
 }
